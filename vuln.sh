f528c6d2a19aeb1bcd20258ef0d471a16c5c2cec
MtheEPIC <mtheepic1423@protonmail.com>
2023-10-22 13:34:40 -0400

    better threading

--- vuln.sh
+++ vuln.sh
@@ -37,13 +37,32 @@ declare -rg SCRIPT_DIR="$(dirname $(readlink -f "$0"))"
 source "$SCRIPT_DIR/utils.sh"
 
 declare -rg LOG_PATH="/var/log/vuln.log"
-declare -rg SCAN_PATH="$(pwd)/vuln_scans" #_$(date +%s)" 
+declare -rg SCAN_PATH="$(pwd)/vuln_scans" #_$(date +%s)"
+declare -rg TEMP_PATH="$(pwd)/temp"
+
 declare -rg USERNAME="${SUDO_USER:-$USER}"
 
 declare -g adapter="eth1"
-declare -g user_list 
-declare -g pass_list
 
+declare -rg DEFAULT_USR_LST="/usr/share/wordlists/metasploit/ipmi_users.txt"
+declare -rg DEFAULT_PAS_LST="/usr/share/wordlists/metasploit/default_pass_for_services_unhash.txt"
+declare -g user_list="$DEFAULT_USR_LST"
+declare -g pass_list=""
+
+declare -rg IP_PATTERN="((25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9][0-9]|[0-9])\.){3}(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9][0-9]|[0-9])"
+declare -rg IS_NUM="^[0-9]+$"
+
+#######TEMP###########
+echo "msfadmin" > ./user.lst
+echo "msfadmin" > ./pass.lst
+user_list="./user.lst"
+pass_list="./pass.lst"
+#######TEMP###########
+
+
+use_user_privileges() {
+	sudo -u "$USERNAME" "$@"
+}
 
 # Check if running as root
 function check_root() {
@@ -115,12 +134,13 @@ parse_adapter() {
 }
 
 parse_report() {
-    local pattern
-    pattern="^((25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9][0-9]|[0-9])\.){3}(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9][0-9]|[0-9])$"
+    local pattern ip
+    #TODO assume pareten is type a/b/c
+    pattern="^${IP_PATTERN}$"
+    ip="$1"
     
     report="aaa"
-    [[ $1 =~ $pattern ]] && report="$1" || { usage; exit 1; }
-    # [ -f "${SCAN_PATH}/${report}" ] #TODO check if file exists
+    [[ ${ip} =~ $pattern ]] && display_findings ${ip} || { usage; exit 1; }
 }
 
 # Function to check if the given user list is valid, if not quit
@@ -151,8 +171,15 @@ init_checks() {
     parse_arguments "$@"
 }
 
+update_db() {
+    (
+        nmap --script-updatedb  
+        searchsploit -u
+    ) >/dev/null
+}
+
 get_valid_adapters() {
-    ip route | grep -o "dev [^ ]*" | cut -d ' ' -f 2 | sort -u
+    ip -4 route | grep -o "dev [^ ]*" | cut -d ' ' -f 2 | sort -u
 }
 
 # Function to check and install required apps
@@ -185,12 +212,14 @@ generate_password_list() {
     
     if [[ "$min_length" -ge "$max_length" || -z "$charset" ]]; then
         echo "Invalid password parameters. Please provide valid values."
-        exit 1
+        return 1
     fi
     
     echo "Generating password list using crunch..."
     crunch "$min_length" "$max_length" "$charset" -o "$pass_list"
     echo "Password list created: $pass_list"
+
+    return 0
 }
 
 # DEPRECATED
@@ -238,28 +267,31 @@ check_pass_list() {
 
 # Function to identify LAN network range
 identify_network_range() {
-    local interface
-    local default_gateway
-    # local network_range
+    local interface default_gateway
+    declare -g gw_adapter
+    
+    title "Identifying LAN network range"
 
     interface="$1"
-
     case "$interface" in #TODO add 'per adapter interface'
         "gateway")
-            default_gateway=$(ip route | awk '{if ($1 ~ /default/) print $5}')
-            network_range=$(ip route | awk -v pattern=$default_gateway '{if ($3 ~ pattern) print $3 ":" $1}')
+            default_gateway=$(ip -4 route | awk '{if ($1 ~ /default/) print $5}')
+            network_range=$(ip -4 route | awk -v pattern=$default_gateway '{if ($3 ~ pattern) print $3 ":" $1}')
             ;;
         "all")
-            network_range=$(ip route | awk '{if ($3 ~ /eth/ || $3 ~ /wlan/) print $3 ":" $1}')
+            network_range=$(ip -4 route | awk '{if ($3 ~ /eth/ || $3 ~ /wlan/) print $3 ":" $1}')
             ;;
         eth*|wlan*)
-            network_range=$(ip route | awk -v pattern=$interface '{if ($3 ~ pattern) print $3 ":" $1}')
+            network_range=$(ip -4 route | awk -v pattern=$interface '{if ($3 ~ pattern) print $3 ":" $1}')
             ;;
         *)
             alert "$interface is an invalid argument"
             exit 1 #FIXME
             ;;
     esac
+    gw_adapter=$(ip -4 route | awk '/default/ {print $5}')
+
+    echo
 }
 
 # Function to perform a quick ARP scan and translate it to IP addresses
@@ -268,10 +300,10 @@ arp_scan() {
     local adapters
     local adapter_sockets=()
 
+    title "Performing a quick ARP scan..."
+
     network_range=$(echo ${1} | sed 's/\n//g')
     IFS=" " read -ra adapter_sockets <<< "$network_range"
-
-    title "Performing a quick ARP scan..."
     
     local adapter element arp_res 
     for element in "${adapter_sockets[@]}"; do
@@ -281,6 +313,7 @@ arp_scan() {
         rhosts+="$arp_res\n"
     done
     rhosts=$(echo -e "$rhosts" | awk '$NF' | sort -u)
+
     echo
 }
 
@@ -296,7 +329,7 @@ enumerate_targets() {
     while IFS= read -r line; do
         enum4linux -a "$line" > "${SCAN_PATH}/${line}.enum"
         #TODO add -p and -u
-        echo
+        echo >> "${SCAN_PATH}/${line}.enum"
     done < "$targets_file"
     
     echo
@@ -304,47 +337,33 @@ enumerate_targets() {
 
 # Function to scan live hosts for open ports
 scan_live_hosts() {
-    local network_range="$1"
+    local network_range
+    network_range="$1"
 
-    local st=$(date +%s)
-
-    if [ "$gw_adapter" == "$adapter" ]
+    # FIXME masscan seems slower then nmap (which should be so)
+    if false #[ "$gw_adapter" == "$adapter" ]
     then
+        #FIXME adapters that aren't gateway don't masscan
         title "Scanning for open ports using masscan..."
 
         #TODO add a spoofed ip using --adapter-ip
-        masscan -Pn -n -iL "$rhosts_file" -p 0-65535 --rate 10000 --retries 1 -oG "grep.txt" # -oX "xml.txt"
+        masscan -Pn -n -iL "$rhosts_file" -p 0-65535 --rate 1000000 --retries 1 -oG "grep.txt" # -oX "xml.txt"
 
         cat grep.txt | awk '{sub(/open.*/, "", $7); print $4" "$7}' | sed 's|/||' | sort -Vu | head -n -3 \
         | awk '{ip[$1] = ip[$1]","$2} END {for (i in ip) print i" "substr(ip[i], 2)}' > ${sockets_file}
+        #TODO add a sockets_file_plus
+        cat grep.txt | awk '{sub("[a-z]*(/){2}", "", $7) sub("/open", "", $7) gsub("/", " ") sub(/\yunknown\y/, ""); print $4" "$7" "$8}' \
+        | sort -Vu | head -n -3 | awk '{ip[$1] = ip[$1]","$2"/"$3} END {for (i in ip) print i" "substr(ip[i], 2)}' | sed 's/,/, /g' > ${sockets_file_plus}
+
     else
         title "Scanning for open ports using Nmap..."
 
         nmap -Pn -n -iL "$rhosts_file" -p- -T4 -oG "grep.txt" &>/dev/null # -oX "xml.txt"
         cat grep.txt | awk '/Ports/ {for (i = 5; i <= NF; i++) sub(/open.*/, "", $i); print}' \
         | sed 's|/ |,|g' | sed -n 's/Host: \([^ ]*\) () Ports: \([^ ]*\),Ignored.*/\1 \2/p' > ${sockets_file}
+        cat grep.txt | awk '/Ports/ {for (i = 5; i <= NF; i++) sub("[a-z]*(/){2}", "", $i) sub("/open", "", $i); print}' \
+        | sed 's|///||g; s/Ignored.*//; s/Host: //; s/() Ports: //' > ${sockets_file_plus}
     fi
-
-
-    # echo "$network_range" > live_hosts.txt
-
-    
-    
-    
-    
-    local ed=$(date +%s)
-    tt=$((ed - st))
-    echo "$tt secs"
-
-
-    # nmap -n -Pn -T4 -p- -iL "$network_range" -oG nmap_live_hosts.txt
-
-	# echo -n "" > ip_port.txt
-	# while IFS= read -r line; do
-	# 	echo $line | grep -Eo '([0-9]+\.[0-9]+\.[0-9]+\.[0-9]+) .*Ports: ([0-9,]+)/' | awk -v ORS='' '{printf "%s:", $1}' >> ip_port.txt 
-	# 	echo $line | grep -Eo '([0-9,]+)/' | tr '\n' ',' | tr -d '/' | sed 's/,$/\n/' >> ip_port.txt
-
-	# done < nmap_live_hosts.txt
 
     echo
 }
@@ -353,7 +372,7 @@ scan_live_hosts() {
 service_scan() {
     # local host="$1"
     # local port="$2"
-    title "perform service scan using Nmap..."
+    title "perform service scan using Nmap..."  
     
 
 	while IFS= read -r line; do
@@ -377,22 +396,123 @@ service_scan() {
 
 }
 
-# Function to brute force weak passwords
-brute_force_passwords() {
-    local host="$1"
-    local user_list="$2"
-    local pass_list="$3"
-    local open_services="$4"
-    
-    for service_port in $open_services; do
-        # Extract the service name and port number
-        service_name="$(echo "$service_port" | cut -d':' -f1)"
-        port_number="$(echo "$service_port" | cut -d':' -f2)"
-        
-        # Perform a brute force attack on the service
-        echo "Brute forcing $service_name on $host:$port_number using Hydra..."
-        hydra -L "$user_list" -P "$pass_list" "$host" "$service_name" -s "$port_number" -o "result_${host}_${port_number}.txt"
+# Function to check for known CVEs based on the service scan
+check_cve() {
+    title "Checking for possible CVE"
+    local file_prefix found_cve cve_payloads cve_sum cve_hosts payload_sum payload_hosts 
+
+    for file in $(find ./ -name "nmap_service_scan_*.txt")
+    do
+        file_prefix=$(echo "$file" | grep -Eo ${IP_PATTERN})
+
+        found_cve=$(cat ${file} | awk '{print $2}' | grep -o "CVE.*" | sort -u)
+        if [ "$found_cve" != "" ]; then
+            echo "$found_cve" > "${file_prefix}_CVE.txt"
+            ((cve_sum += $(echo "$found_cve" | wc -l)))
+            ((cve_hosts += 1))
+
+            cve_payloads=$(echo ${found_cve} | xargs -I{} searchsploit {} | grep -v "No Results")
+            if [ "$cve_payloads" != "" ]; then
+                echo ${cve_payloads} > "${file_prefix}_CVE_payloads.txt"
+                ((payload_sum += $(echo "$cve_payloads" | wc -l)))
+                ((payload_hosts += 1))
+            fi
+        fi
+
+        #TODO download https://vulners.com/seebug exploits from the ${file}
+        # cat nmap_service_scan_10.0.0.66.xml | grep "service name.*version[^ ]*" -o
     done
+
+    [[ ${cve_sum} =~ ${IS_NUM} ]] && note "found ${cve_sum} CVEs in ${cve_hosts} targets"
+    [[ ${payload_sum} =~ ${IS_NUM} ]] && note "searchsploit found ${payload_sum} payloads for ${payload_hosts} targets"
+
+    echo
+}
+
+find_payloads() {
+    title "Checking for searchploit payloads for services"
+    local file_prefix payloads_file found_services found_payloads payload_sum payload_hosts 
+
+    for file in $(find ./ -name "nmap_service_scan_*.txt")
+    do
+        file_prefix=$(echo "$file" | grep -Eo ${IP_PATTERN})
+        payloads_file="${file_prefix}_payloads.txt"
+        echo > ${payloads_file}
+
+        found_services=$(cat "$file" | grep -E "^[0-9]+/" | awk '{ for (i = 4; i <= NF; i++) printf $i " "; print "" }' | awk 'NF' | sort -uV)
+        
+        while IFS= read -r service; do 
+            res=""
+            while [ -z "$res" ] && [ -n "$service" ] ; do
+                res=$(searchsploit "$service" 2>/dev/null | grep -v "No Results" 2>/dev/null)
+                service="$(echo "$service" | awk '{ for (i = 1; i < NF; i++) printf $i " "; print "" }')"
+
+                #TODO check is service was alredy used (even after sort-u differbt services may have a same prefix)
+                # temp fix: cat payloads_file | sort -u 
+            done
+            [ -z "$res" ] && continue
+
+            echo "$res" >> ${payloads_file}
+        done <<< "$found_services"
+
+        found_payloads=$(cat ${payloads_file} | sort -u)
+        [ -z "$found_payloads" ] && continue
+
+        echo "$found_payloads" > ${payloads_file}
+        ((payload_sum += $(echo "$found_payloads" | wc -l)))
+        ((payload_hosts += 1))
+    done
+
+    [[ ${payload_sum} =~ ${IS_NUM} ]] && note "searchsploit found ${payload_sum} payloads for ${payload_hosts} targets"
+    echo
+}
+
+# Function to check for the use of weak passwords
+online_attack() {
+    title "Attempting a bruth force attack"
+
+    while IFS= read -r line; do
+        local ip services service port
+
+        # if there are no valid port services contine to the next target
+        services=$(echo "$line" | grep -E "[0-9]+/[a-z|-]+" -o)
+        [ -n "$services" ] && service=$(echo "$services" | awk 'NR==1') || continue
+
+        ip=$(echo "$line" | awk '{print $1}')
+        port=$(echo "$service" | cut -d '/' -f 1)
+        service=$(echo "$service" | cut -d '/' -f 2)
+        
+        # translate specific service name into a generic version
+        case "$service" in
+            msrpc)
+                service="smb"
+                ;;
+            *)
+                ;;
+        esac
+
+        brute_force_passwords ${ip} ${service} ${port}
+	done < ${sockets_file_plus}
+
+    echo
+}
+
+# Function to brute force a given socket
+brute_force_passwords() {
+    local ip service port result
+    ip="$1"
+    service="$2"
+    port="$3"
+
+    # local user_list="$2"
+    # local pass_list="$3"
+    # local open_services="$4"
+
+    # Perform a brute force attack on the service
+    note "Brute forcing $service on $ip:$port"
+    use_user_privileges hydra -L "$user_list" -P "$pass_list" "$ip" "$service" -s "$port" -o "hydra_${ip}_${service}.txt" &>/dev/null
+    result="$(grep -w login hydra_${ip}_${service}.txt | tail -n 1)"
+    [ -n "$result" ] && success "$result" || alert "attack failed"
 }
 
 # Function to display general statistics
@@ -419,10 +539,38 @@ save_report() {
 
 # Function to display relevant findings for a given IP address
 display_findings() {
-    local ip="$1"
-    grep -A 1 "$ip" nmap_live_hosts.txt
-    grep "$ip" vulnerabilities.txt
-    grep "$ip" result_*
+    local ip report_file
+    ip="$1"
+
+    report_file="${SCAN_PATH}/${ip}_report.txt"
+    
+    # [ -f "${SCAN_PATH}/${report}" ] #TODO check if file exists
+    check_readable ${report_file} || { [ $? -eq 1 ] && exit 1; }
+    cat ${report_file}
+
+    # grep -A 1 "$ip" nmap_live_hosts.txt
+    # grep "$ip" vulnerabilities.txt
+    # grep "$ip" result_*
+}
+
+function with_loading_animation() {
+    local padding msg
+    padding="    "
+    msg="$1"
+    shift
+
+    # echo -ne "${GREEN}[+] ${CYAN}"
+    cycle_word_and_chars "${TITLE_PREFIX} ${TITLE_MSG}$msg${NC}" &
+
+    # title "$msg" | cycle_word_and_chars &
+
+    local loading_msg_pid=$!
+
+	"$@" &>/dev/null  # Suppress the command's output
+    sleep 10
+    kill $loading_msg_pid
+    echo -ne "\r" #${GREEN}[+] ${CYAN}"
+    title "${msg}${padding}"
 }
 
 # Main function
@@ -431,53 +579,50 @@ main() {
     declare -rg rhosts_file="${SCAN_PATH}/rhosts.txt"
     declare -rg enums_file="${SCAN_PATH}/enum.txt"
     declare -rg sockets_file="${SCAN_PATH}/open_sockets.txt"
+    declare -rg sockets_file_plus="${SCAN_PATH}/open_sockets_service.txt"
+
+    local start_time end_time
 
     init_checks "$@"
 
-    [ -z "$pass_list" ] && generate_password_list
+    # update_db
 
-    # start_time=$(date +%s)
+    if [ -z "$pass_list" ]; then
+        while true; do
+            generate_password_list
 
-    
-    identify_network_range "$adapter" #FIXME not gateway doesnt masscan
-    declare -g gw_adapter=$(ip route | awk '/default/ {print $5}')
-    # echo "$network_range"
+            # [ $? -eq 0 ] && break
+            break
+        done
+    fi
 
-    arp_scan "$network_range"
+    start_time=$(date +%s)
+
+    # adapter="eth0"
+    with_loading_animation "Identifying LAN network range" identify_network_range "$adapter"
+    with_loading_animation "Performing a quick ARP scan... " arp_scan "$network_range"
     #TODO maybe add ping scan?
-    # echo -e "$rhosts"
-    echo -e "$rhosts" > "$rhosts_file"
+    use_user_privileges echo -e "$rhosts" > "$rhosts_file"    
 
-    enumerate_targets "$rhosts_file" &
-    local loading_msg_pid=$!
+    run_in_background enumerate_targets ${rhosts_file}
     
-    scan_live_hosts "$rhosts"
+    with_loading_animation "Scanning for open ports using Nmap... " scan_live_hosts "$rhosts"
 
-	service_scan
+	# service_scan
 
-    # searchsploit -u 
-    # nmap --script-updatedb    
-    # nmap --script "*vuln*
+    # Step 2
+    run_in_background check_cve
+    run_in_background find_payloads
+    run_in_background online_attack
+    # online_attack
 
-
-    wait $loading_msg_pid
-
-    success "done"
+    # figlet "wait"
+    wait_for_all_background
 	exit 0
-
-    for file in $(find ./ -name "nmap_service_scan_*.txt")
-    do
-        # cat ${file} | awk '{print $2}' | grep -o "CVE.*" | sort -u | xargs -I{} searchsploit {} 
-        #TODO download https://vulners.com/seebug exploits from the ${file}
-        # cat nmap_service_scan_10.0.0.66.xml | grep "service name.*version[^ ]*" -o
-        echo $file
-    done
 
     display_statistics
     save_report
-
-    read -rp "Enter an IP address to display relevant findings: " ip_address
-    display_findings "$ip_address"
+    success "done"
 }
 
 # Call the main function and pass all script arguments
